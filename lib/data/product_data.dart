665f4c7626d829cc451cf0fafa89a5e4dfd7d114
Aniket Tiwari <aniket120208@gmail.com>
2026-09-19 09:39:59 +0530

    feat: add product details

---
+++ lib/data/product_data.dart
@@ -0,0 +1,34 @@
+const List<Map<String, String>> products = [
+    {
+      'badge': '-30% OFF',
+      'brand': 'ZORIX STUDIO',
+      'title': 'Structured Wool Coat',
+      'price': '\$221',
+      'originalPrice': '\$340',
+      'image': 'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?w=500&q=80',
+    },
+    {
+      'badge': '-40% FLASH',
+      'brand': 'EDITORIAL',
+      'title': 'Pleated Wide-Leg Pants',
+      'price': '\$126',
+      'originalPrice': '\$210',
+      'image': 'https://images.unsplash.com/photo-1509631179647-0177331693ae?w=500&q=80',
+    },
+    {
+      'badge': 'BEST VALUE',
+      'brand': 'KNIT ARCHIVE',
+      'title': 'Cashmere Mockneck',
+      'price': '\$136',
+      'originalPrice': '\$195',
+      'image': 'https://images.unsplash.com/photo-1576995853123-5a10305d93c0?w=500&q=80',
+    },
+    {
+      'badge': '25% LIMITED',
+      'brand': 'LEATHERWOODS',
+      'title': 'Leather Crescent Bag',
+      'price': '\$210',
+      'originalPrice': '\$280',
+      'image': 'https://images.unsplash.com/photo-1548036328-c9fa89d128fa?w=500&q=80',
+    },
+  ];
