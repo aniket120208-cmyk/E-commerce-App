38f31f8ce3f771299b3a2c6704614ff88b6587a6
Aniket Tiwari <aniket120208@gmail.com>
2026-09-19 21:26:54 +0530

    feat: add more product model

---
+++ lib/models/product_model.dart
@@ -0,0 +1,63 @@
+class Product {
+  final String id;
+  final String brand;
+  final String title;
+  final String description;
+  final String price;
+  final String originalPrice;
+  final String? badge;
+  final String image;
+  final List<String> sizes;
+  final List<String> colors;
+  final String material;
+  final String care;
+
+  const Product({
+    required this.id,
+    required this.brand,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.originalPrice,
+    this.badge,
+    required this.image,
+    required this.sizes,
+    required this.colors,
+    required this.material,
+    required this.care,
+  });
+
+  factory Product.fromMap(Map<String, dynamic> map) {
+    return Product(
+      id: map['id'] as String,
+      brand: map['brand'] as String,
+      title: map['title'] as String,
+      description: map['description'] as String,
+      price: map['price'] as String,
+      originalPrice: map['originalPrice'] as String,
+      badge: map['badge'] as String?,
+      image: map['image'] as String,
+      sizes: List<String>.from(map['sizes'] as List),
+      colors: List<String>.from(map['colors'] as List),
+      material: map['material'] as String,
+      care: map['care'] as String,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'brand': brand,
+      'title': title,
+      'description': description,
+      'price': price,
+      'originalPrice': originalPrice,
+      'badge': badge,
+      'image': image,
+      'sizes': sizes,
+      'colors': colors,
+      'material': material,
+      'care': care,
+    };
+  }
+}
